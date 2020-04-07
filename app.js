9794c0daa12bf0aaa3c43ecc275cc53ee192768b
Daistudio <claussremi@gmail.com>
2020-04-07 18:16:23 +0200

    Ajout du code barba.js

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-Barba.Pjax.start();
+// do not import Barba like this if you load the library through the browser
+import barba from '@barba/core';
+
+// basic default transition (with no rules and minimal hooks)
+barba.init({
+    transitions: [{
+        leave({ current, next, trigger }) {
+            // do something with `current.container` for your leave transition
+            // then return a promise or use `this.async()`
+        },
+        enter({ current, next, trigger }) {
+            // do something with `next.container` for your enter transition
+            // then return a promise or use `this.async()`
+        }
+    }]
+});
+
+// dummy example to illustrate rules and hooks
+barba.init({
+    transitions: [{
+        name: 'dummy-transition',
+
+        // apply only when leaving `[data-barba-namespace="home"]`
+        from: 'home',
+
+        // apply only when transitioning to `[data-barba-namespace="products | contact"]`
+        to: {
+            namespace: [
+                'products',
+                'contact'
+            ]
+        },
+
+        // apply only if clicked link contains `.cta`
+        custom: ({ current, next, trigger }) => trigger.classList && trigger.classList.contains('cta'),
+
+        // do leave and enter concurrently
+        sync: true,
+
+        // available hooks…
+        beforeOnce() {},
+        once() {},
+        afterOnce() {},
+        beforeLeave() {},
+        leave() {},
+        afterLeave() {},
+        beforeEnter() {},
+        enter() {},
+        afterEnter() {}
+    }]
+});
